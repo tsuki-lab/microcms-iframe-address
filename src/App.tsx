b689232542a31c8194d1924f5997be7c530ff8d1
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-04-30 09:08:40 +0900

    manage formState

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,103 @@
-import { useMemo, useRef } from 'react'
+import { Reducer, useEffect, useMemo, useReducer } from 'react'
 import { Loading } from './components/Loading'
-import { useFetchPrefectures } from './hooks/use-fetch-prefectures'
-import { useZipcloud } from './hooks/use-zipcloud'
+import { useMicroCMSIframe } from './hooks/use-microcms-iframe'
+import { useZipcloud } from './hooks/use-zipcloud/use-zipcloud'
 
 import './app.css'
 
-function App() {
-  const {error: zipError, handler, loading} = useZipcloud()
-  const isLoading = useMemo(() => {
-    return loading
-  }, [loading])
+type FormState = {
+  zip: string,
+  address: string,
+  building: string
+}
 
-  const zipRef = useRef<HTMLInputElement>(null)
-  const addressRef = useRef<HTMLInputElement>(null)
-  const building = useRef<HTMLInputElement>(null)
+type FormReducer = Reducer<FormState, Partial<FormState>>
+
+function App() {
+  const { state, postHandler } = useMicroCMSIframe<FormState>({
+    height: 410
+  })
+  const { error: zipError, handler, loading } = useZipcloud()
+
+  const isLoading = useMemo(() => loading, [loading])
+
+  const [formState, formDispatch] = useReducer<FormReducer>((prev, action) => {
+    return { ...prev, ...action }
+  }, {
+    zip: '',
+    address: '',
+    building: ''
+  })
 
   const searchZip = async () => {
-    if (!addressRef.current) return
-
-    const value = zipRef.current?.value || ''
-    const { data: zips } = await handler(value)
-    if (!zips) return
-
-    addressRef.current.value = `${zips[0].address1}${zips[0].address2}${zips[0].address3}`
+    const { data: zips } = await handler(formState.zip)
+    if (zips) {
+      formDispatch({address: `${zips[0].address1}${zips[0].address2}${zips[0].address3}`})
+    }
   }
 
-  const submitHandler = async(e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-  }
+  useEffect(() => {
+    if (state.defaultMessage?.data) {
+      formDispatch(state.defaultMessage.data)
+    }
+  }, [state.defaultMessage])
+
+  useEffect(() => {
+    const values = [formState.zip.replace(/([0-9]{3})([0-9]{4})/, ''), formState.address, formState.building]
+    postHandler({
+      description: values.join(' '),
+      data: formState
+    })
+  }, [formState])
 
   return (
-    <main>
-      <form onSubmit={submitHandler}>
-
+    <>
+      <form>
         <label htmlFor="zip">
-          郵便番号
-          <span className='required'>*</span><br />
+          郵便番号<br />
           <span className='example'>1640001</span>
         </label>
 
-        <input id="zip" type="text" ref={zipRef} required autoComplete='off' />
-        <p className="red">{zipError}</p>
+        <input
+          id="zip"
+          type="text"
+          required
+          autoComplete='off'
+          value={formState.zip}
+          onChange={(e) => formDispatch({zip: e.target.value})}
+        />
+        <p className="error">{zipError}</p>
         <button type='button' onClick={searchZip}>郵便番号で住所を検索</button>
 
         <label htmlFor="address">
-          住所
-          <span className='required'>*</span><br />
+          住所<br />
           <span className='example'>東京都中野区中野5-65-5</span>
         </label>
-        <input id="address" type="text" ref={addressRef} className="w-full" required />
+        <input
+          id="address"
+          type="text"
+          className="w-full"
+          required
+          autoComplete='off'
+          value={formState.address}
+          onChange={(e) => formDispatch({address: e.target.value})}
+        />
 
         <label htmlFor="building">
           建物名<br />
           <span className='example'>豊島興業ビル7F</span>
         </label>
-        <input id="building" type="text" ref={building} className="w-full" />
+        <input
+          id="building"
+          type="text"
+          className="w-full"
+          autoComplete='off'
+          value={formState.building}
+          onChange={(e) => formDispatch({building: e.target.value})}
+        />
       </form>
       {isLoading && <Loading />}
-    </main>
+    </>
   )
 }
 
